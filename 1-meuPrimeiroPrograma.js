55c5fbe2a93831bacc6986ac4b01d0cdd7108453
lauxenmaju <lauxen.maria@escola.pr.gov.br>
2024-09-16 08:53:21 -0300

    first commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,17 @@
+console.log("Olá mundo cruel da programação");
+
+const aluno1 = "Jose"
+const aluno2 = "Deivid"
+
+const idadeAluno1 = 16
+const idadeAluno2 = 16
+
+const somaIdades = +idadeAluno1+idadeAluno2
+
+const dupla = "Nós somos a dupla: "+aluno1+ " e "+ aluno2
+
+console.log("A soma de nossas idades é "+somaIdades)
+console.log(dupla)
+
+document.write("A soma de nossas idades é "+somaIdades+" ")
+document.write(dupla)
